5396722b3ccf20ac0ab21856b55095841c6e08ea
Matvei <matvesch@protonmail.com>
2023-04-20 14:05:35 +0300

    auth

--- src/routes/noteRoutes.ts
+++ src/routes/noteRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { create_note, delete_note, get_notes } from '../controllers/noteController';
+import passport from 'passport';
 
 const router = express.Router();
 
-router.get('/', get_notes);
-router.post('/create', create_note);
-router.delete('/:noteId', delete_note);
+router.get('/', passport.authenticate('jwt', { session: false }), get_notes);
+router.post('/create', passport.authenticate('jwt', { session: false }), create_note);
+router.delete('/:noteId', passport.authenticate('jwt', { session: false }), delete_note);
 
 export default router;
